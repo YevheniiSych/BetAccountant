3b5e0d46001b109b8ebb08dae9fbb7af37b8dc9e
Yevhenii <ysych@masslight.com>
2021-04-07 12:17:40 +0300

    Added fifth level wrong answer dialog.

--- app/src/main/java/com/betaccountant/fragment/FourthLevel.kt
+++ app/src/main/java/com/betaccountant/fragment/FourthLevel.kt
@@ -1,12 +1,13 @@
 package com.betaccountant.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.betaccountant.MainActivity
 import com.betaccountant.R
+import com.betaccountant.dialog.StoryDialog
 import com.betaccountant.enums.Level
 import kotlinx.android.synthetic.main.fragment_fourth_level.*
 
@@ -23,9 +24,18 @@ class FourthLevel : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         finishGameButton.setOnClickListener {
-            if(inputCode.text.toString() == ANSWER_CODE){
+            if (inputCode.text.toString() == ANSWER_CODE) {
                 (activity as MainActivity).showPromoVideoAndNavigateToLevel(Level.FIFTH)
+            } else {
+                showWrongAnswerDialog()
             }
         }
+    }
+
+    private fun showWrongAnswerDialog() {
+        StoryDialog(
+            requireContext(),
+            getString(R.string.third_level_wrong_answer), {}
+        ).show()
     }
 }
